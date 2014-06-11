0bb35722ddaa02f763b0e09c35737b05b2884fb0
Geoff Wagstaff <geoffwagstaff@gmail.com>
2014-06-11 21:01:52 +0100

    Obtain redis from function / opts

--- test/anchorman.js
+++ test/anchorman.js
@@ -1,11 +1,16 @@
 var Anchorman = require('../lib/Anchorman');
 var should = require('should');
+var redis = require('redis');
+
+var createRedisClient = function() {
+  return redis.createClient();
+};
 
 describe('Anchorman', function() {
-  var anchorman = Anchorman();
+  var anchorman = Anchorman(createRedisClient);
 
   before(function(done) {
-    anchorman.once('ready', done);
+    anchorman.checkReady(done);
   });
 
   it('receives messages when published', function(done) {
